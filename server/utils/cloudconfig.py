5a2ea1036724c19b59c2f26a4f26b460b2b28f79
Abel Koech <koechabel161@gmail.com>
2024-10-15 19:13:14 +0300

    delete utils file

--- server/utils/cloudconfig.py
+++
@@ -1,8 +0,0 @@
-# import cloudinary
-
-# cloudconfig = cloudinary.config(
-#      cloud_name = "dmsnelbpc", 
-#     api_key = "176473414358681", 
-#     api_secret = "c5Vamh25mwkkDXwMw3zlKh1_mic",
-#     secure=True
-# )
