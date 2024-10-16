5fc9df4426156ddeac0ec0d2b1e9d373927ab60f
Abel Koech <koechabel161@gmail.com>
2024-10-16 17:33:28 +0300

    solved client side

--- client/src/App.js
+++ client/src/App.js
@@ -5,33 +5,11 @@ import './App.css';
 
 
 function App() {
-  const [books, setBooks] = useState({})
-
-  useEffect(() =>{
-    fetch('/books')
-    .then((res) => res.json())
-    .then((data) => {
-      setBooks(data)
-      console.log(data);
-      
-    })
-  },[])
+ 
   return (
     <div className="App">
    
-     <div className='row g-3'>
-  {books.length > 0 ? (
-    books.map((book) => (
-      <div className='col-md-4' key={book.id}>
-        <img  className='img-fluid' src={book.image_filename} alt='alt'/>
-        <h1>{book.title}</h1>
-        <button className='btn bg-success text-white '>Add Review</button>
-      </div>
-    ))
-  ) : (
-    <p>No books found.</p>
-  )}
-</div>
+    
 
   
     </div>
