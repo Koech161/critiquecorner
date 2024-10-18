1bf3ea65da0463cc67cb6f4bcfde8231da8f45f1
Abel Koech <koechabel161@gmail.com>
2024-10-19 01:45:39 +0300

    troubleshooting

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,14 +2,12 @@
 import React from 'react';
 import './Homepage.css';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthProvider';
-import { useUser } from './UserContext';
+
 
 
 const Homepage = () => {
   const navigate = useNavigate();
-  const { currentUser } = useUser()
-  const { isAuthenticated } = useAuth()
+  
 
   const handleExploreButton = () => {
     navigate('/books');
@@ -19,7 +17,7 @@ const Homepage = () => {
     <div 
       className=" homepage container text-center mt-5" 
     >
-      <h1 className="display-4 text-white bg-secondary">{isAuthenticated ? `${currentUser.username}`: ''} Welcome to CritiqueCorner the Book Review Website</h1>
+      <h1 className="display-4 text-white bg-secondary"> Welcome to CritiqueCorner the Book Review Website</h1>
       <p className="lead text-white bg-secondary">Explore our collection of books and reviews. Join our community to share your thoughts!</p>
       <button 
         className="btn btn-primary btn-lg mt-4" 
